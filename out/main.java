4202830ba9461eac75b30243f66816e7863ae67d
deniscozma2019 <deniscozma2015@gmail.com>
2025-08-29 15:06:01 +0300

    test git connection

---
+++ out/main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        int sum = a + b + c;
+        System.out.println("Sum: " + sum);
+    }
+}
